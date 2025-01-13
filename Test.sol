86a4864ade01c013256205a821a1a0e4c22a10f5
tajdine-software-engineer <tajdine.elm@gmail.com>
2025-01-13 13:59:42 +0300

    Test

--- Test.sol
+++ Test.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.26;
+pragma solidity >=0.5.8 <=0.8.26;
 
 contract Test{
-    uint public a = 1;
+    uint8 public a = 1;
 
-    function set(uint _a) public{
-        a = _a;
+    function set() public{
+        a += 1;
     }
 }
